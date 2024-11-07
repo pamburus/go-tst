29b50308909afb0d1108aee2329349e2eb887d6f
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-11-07 23:25:39 +0100

    feat: renamed `Base` to `HT` (#21)

--- tst/constraint.go
+++ tst/constraint.go
@@ -4,8 +4,8 @@ import "testing"
 
 // ---
 
-// Base is constraint for a hierarchical test base.
-type Base[T testing.TB] interface {
+// HT is constraint for a any hierarchical test compatible with [testing.TB].
+type HT[T testing.TB] interface {
 	testing.TB
 	Run(name string, f func(T)) bool
 }
