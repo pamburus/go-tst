e488ade8222e84bbab884404d3c633cab2622694
Pavel Ivanov <mr.pavel.ivanov@gmail.com>
2024-11-08 13:27:41 +0100

    feat(tst): improved line tag reporting

--- tst/constraint.go
+++ tst/constraint.go
@@ -7,5 +7,7 @@ import "testing"
 // HT is constraint for a any hierarchical test compatible with [testing.TB].
 type HT[T testing.TB] interface {
 	testing.TB
+
+	// Run runs a subtest with the given name and function.
 	Run(name string, f func(T)) bool
 }
